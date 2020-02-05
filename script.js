aaa0991285d2c7d31b1a63237f1239625abe93c7
Alex Lavoie <alexflavoie@gmail.com>
2020-02-04 17:46:25 -0800

    Finished lab 4

--- script.js
+++ script.js
@@ -8,6 +8,57 @@ function toggleForm() {
     }
 }
 
+function deleteArtist(id){
+    window.localStorage.removeItem(id.toString());
+    document.getElementById(id.toString()).remove();
+}
+
+function loadArtists(){
+    let ids = window.localStorage.getItem("idInc");
+    if(ids !== undefined) {
+        ids = Number(ids);
+        let artists = document.getElementById("artists");
+        for (let i = 0; i <= ids; i++) {
+            let artistInfo = JSON.parse(window.localStorage.getItem(i.toString()));
+            if(artistInfo !== null) {
+                let id = artistInfo[0];
+                let name = artistInfo[2];
+                let about = artistInfo[3];
+                let image = artistInfo[1];
+
+                let newArtist = document.createElement("li");
+                let div = document.createElement("div");
+                let nameElement = document.createElement("h3");
+                let pic = document.createElement("img");
+                let aboutElement = document.createElement("h5");
+                let deleteButton = document.createElement("input");
+
+                newArtist.className = 'artist';
+                newArtist.id = id;
+                pic.src = image;
+                pic.alt = "Artist Image";
+                nameElement.className = "text";
+                nameElement.textContent = name;
+                aboutElement.className = "text";
+                aboutElement.textContent = about;
+                deleteButton.className = "deleteArtist";
+                deleteButton.type = 'button';
+                deleteButton.value = 'Delete';
+                deleteButton.addEventListener("click", f => {
+                    deleteArtist(newArtist.id)
+                });
+
+                newArtist.appendChild(pic);
+                div.appendChild(nameElement);
+                div.appendChild(aboutElement);
+                div.appendChild(deleteButton);
+                newArtist.appendChild(div);
+                artists.appendChild(newArtist);
+            }
+        }
+    }
+}
+
 function addArtist(){
     try {
         let artists = document.getElementById("artists");
@@ -37,7 +88,6 @@ function addArtist(){
         deleteButton.className = "deleteArtist";
         deleteButton.type = 'button';
         deleteButton.value = 'Delete';
-        deleteButton.addEventListener("click", f => {newArtist.remove()});
 
         newArtist.appendChild(pic);
         div.appendChild(nameElement);
@@ -46,10 +96,47 @@ function addArtist(){
         newArtist.appendChild(div);
         artists.appendChild(newArtist);
 
+        if(window.localStorage.getItem("idInc") != undefined){
+            let id = Number(window.localStorage.getItem("idInc"));
+            id++;
+            newArtist.id = id.toString();
+            window.localStorage.setItem("idInc", id.toString());
+        } else {
+            newArtist.id = "0";
+            window.localStorage.setItem("idInc", "0");
+        }
+
+        deleteButton.addEventListener("click", f => {deleteArtist(newArtist.id)});
+
+        let artistInfo = [Number(newArtist.id), pic.getAttribute("src"),
+            name.value, about.value];
+        window.localStorage.setItem(newArtist.id, JSON.stringify(artistInfo));
+
         name.value = '';
         about.value = '';
         image.value = '';
+
     } catch {
         alert("Error adding artist. Please try again.");
+    }
+}
+
+function searchArtist(){
+    let search = document.getElementById("search");
+    let artists = document.getElementById("artists");
+    if(search.value != ""){
+        for(let i = 0; i < artists.children.length; i++){
+            let artist = artists.children[i];
+            let name = artist.children[1].children[0];
+            if(!name.textContent.toLowerCase().includes(search.value.toLowerCase())){
+                artist.style.display = 'none';
+            } else {
+                artist.style.display = 'block';
+            }
+        }
+    } else {
+        for(let i = 0; i < artists.children.length; i++){
+            artists.children[i].style.display = 'block';
+        }
     }
 }
