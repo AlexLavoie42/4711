41c0e1de3acf890fc6ceb334566f631b08d57e69
AlexLavoie <lavoiealex@hotmail.com>
2020-03-16 18:14:58 -0700

    """Finished""" lab 6

--- util/database.js
+++ util/database.js
@@ -1,4 +1,4 @@
 const mysql = require("mysql2");
 
-pool = mysql.createPool({host: 'localhost', user: 'root', database: 'artistdb', password: 'VancouveR420'});
+pool = mysql.createPool({host: 'remotemysql.com', user: 'HQF7vnsZyq', database: 'HQF7vnsZyq', password: 'W8T5dtqurR'});
 module.exports = pool.promise();
